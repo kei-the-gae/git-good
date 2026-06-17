7963ebbb6cb3d3e8cc6674acecda999ffba5a3d2
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2026-06-16 22:25:19 -0700

    fix images on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://kei-the-gae.github.io",
+    base: "/git-good",
     integrations: [
         starlight({
             title: "Git Good",
